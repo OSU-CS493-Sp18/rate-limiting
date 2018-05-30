a85d9457e48a0de40e64b89ddba189515372230a
Rob Hess <robwhess@gmail.com>
2018-05-30 16:31:44 -0700

    Add code from 3/30 lecture.

--- server.js
+++ server.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT || '6379';
+const redisClient = redis.createClient(redisPort, redisHost);
+
+function getUserTokenBucket(ip) {
+  return new Promise((resolve, reject) => {
+    redisClient.hgetall(ip, function (err, tokenBucket) {
+      if (err) {
+        reject(err);
+      } else {
+        if (tokenBucket) {
+          tokenBucket.tokens = parseFloat(tokenBucket.tokens);
+        }
+        resolve(tokenBucket);
+      }
+    });
+  });
+}
+
+function rateLimit(req, res, next) {
+  const windowMilliseconds = 60000;
+  const windowMaxRequests = 5;
+  const now = Date.now();
+  getUserTokenBucket(req.ip)
+    .then((tokenBucket) => {
+      if (!tokenBucket) {
+        tokenBucket = { tokens: windowMaxRequests, last: now };
+      }
+    })
+    .catch((err) => {
+      next();
+    });
+}
+
+app.use(rateLimit);
 
 app.get('/', function (req, res) {
   res.status(200).json({
