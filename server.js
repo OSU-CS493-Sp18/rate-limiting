a9bee7a13b578a09c1a6ef0a27f1f08e2618d7d7
Rob Hess <robwhess@gmail.com>
2018-05-30 13:05:04 -0700

    Add initial code.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', function (req, res) {
+  res.status(200).json({
+    timestamp: new Date().toString()
+  });
+});
+
+app.use('*', function (req, res, next) {
+  res.status(404).json({
+    err: "Path " + req.originalUrl + " does not exist"
+  });
+});
+
+  app.listen(port, function() {
+    console.log("== Server is running on port", port);
+  });
